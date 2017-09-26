96402a26fd2b67faa7b77b43244558d1754c00c5
nail60 <nail60@gmail.com>
2017-09-26 00:01:33 -0700

    more silly optomizations to day5

--- day5/sketch.js
+++ day5/sketch.js
@@ -15,25 +15,32 @@ function setup() {
   background(60);
   sinX = 0;
   sinY = windowHeight/2;
+  from = color(200, 200, 32);
+  to = color(72, 61, 200);
   for(var i=0; i < windowWidth; i++){
     endPoints1.push( [ i+cos(radians((i*5)%360)) , windowHeight/2+sin(radians((i*5)%360))*100 ] );
     endPoints2.push( [ i+2+cos(radians((i*5)%360)) , windowHeight/2-50-sin(radians((i*5)%360))*100 ] );
   }
+  drawLines();
 }
 
 function draw() {
-  fill(90);
-  rect(0,0,windowWidth,windowHeight);
-  from = color(200, 200, 32);
-  to = color(72, 61, 200);
+
+}
+
+function drawLines(){
   for(var i=0; i < windowWidth; i++){
     var lerp = map(i, 0, windowWidth, 0, 1);
     stroke(lerpColor(from, to, lerp));
     line(0+i*mouseX/100, 0, endPoints1[i][0], endPoints1[i][1]);
     line(0+i*mouseX/100, windowHeight, endPoints2[i][0], endPoints2[i][1]);
   }
+}
 
-
+function mouseMoved(){
+  fill(90);
+  rect(0,0,windowWidth,windowHeight);
+  drawLines();
 }
 
 function windowResized() {
