04bee3c975ae53e37deaa614bdbb324d86707df6
nail60 <nail60@gmail.com>
2017-09-25 23:51:10 -0700

    Optomized code for day 5

--- day5/sketch.js
+++ day5/sketch.js
@@ -7,12 +7,18 @@ var elements;
 
 var sinX;
 var sinY;
+var endPoints1 = [];
+var endPoints2 = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(60);
   sinX = 0;
   sinY = windowHeight/2;
+  for(var i=0; i < windowWidth; i++){
+    endPoints1.push( [ i+cos(radians((i*5)%360)) , windowHeight/2+sin(radians((i*5)%360))*100 ] );
+    endPoints2.push( [ i+2+cos(radians((i*5)%360)) , windowHeight/2-50-sin(radians((i*5)%360))*100 ] );
+  }
 }
 
 function draw() {
@@ -23,8 +29,8 @@ function draw() {
   for(var i=0; i < windowWidth; i++){
     var lerp = map(i, 0, windowWidth, 0, 1);
     stroke(lerpColor(from, to, lerp));
-    line(0+i*mouseX/100, 0, i+cos(radians((i*5)%360)), windowHeight/2+sin(radians((i*5)%360))*100);
-    line(0+i*mouseX/100, windowHeight, i+2+cos(radians((i*5)%360)), windowHeight/2-50-sin(radians((i*5)%360))*100);
+    line(0+i*mouseX/100, 0, endPoints1[i][0], endPoints1[i][1]);
+    line(0+i*mouseX/100, windowHeight, endPoints2[i][0], endPoints2[i][1]);
   }
 
 
